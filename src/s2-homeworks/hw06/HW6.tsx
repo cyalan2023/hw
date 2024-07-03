c61c161eb714ad5b68f873c3340adc7036220ea2
Alex <cyalan2019@gmail.com>
2024-07-03 15:28:00 +0500

    first

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,7 +19,8 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        const state = restoreState<string>("hw6-editable-span-value", "");
+        setValue(state);
     }
 
     return (
